47e13e99da71ef040e38d38eef0c394deb178080
Helton Quezada <info.johnjairo@gmail.com>
2021-10-06 20:35:52 -0500

    En esta version esta incluido talwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,16 @@
-const { guessProductionMode } = require("@ngneat/tailwind");
-
-process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
-
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-};
+  important: true,
+  purge: [{
+    enabled: true,
+    content: ['./src/**/*.{html,ts}'],
+  }
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
